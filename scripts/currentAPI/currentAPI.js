74de49bda025c6d290140279d0dcf87f7bb70aad
David Rowe <david@ctrlaltstudio.com>
2018-01-15 14:23:43 +1300

    ESLint

--- scripts/currentAPI/currentAPI.js
+++ scripts/currentAPI/currentAPI.js
@@ -1,42 +1,58 @@
-//
-//  currentAPI.js
-//  examples
-//
-//  Created by Clément Brisset on 5/30/14.
-//  Copyright 2014 High Fidelity, Inc.
-//
-//  Distributed under the Apache License, Version 2.0.
-//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-//
+/*!
+currentAPI.js
 
-var array = [];
-function listKeys(string, object) {
-    if (string === "listKeys" || string === "array" || string === "buffer" || string === "i") {
-        return;
-    }
-    
-    if (typeof(object) !== "object" || object === null) {
-        array.push(string + " " + typeof(object));
-        return;
-    }
-    
-    var keys = Object.keys(object);
-    for (var i = 0; i < keys.length; ++i) {
-        if (string === "") {
-            listKeys(keys[i], object[keys[i]]);
-        } else if (keys[i] !== "parent") {
-            listKeys(string + "." + keys[i], object[keys[i]]);
+Created by Clément Brisset on 5/30/14.
+Copyright 2014 High Fidelity, Inc.
+
+Modified by David Rowe:
+- ESLint.
+
+Distributed under the Apache License, Version 2.0.
+See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+*/
+
+(function () {
+
+    "use strict";
+
+    var array = [],
+        buffer,
+        i,
+        length;
+
+    function listKeys(string, object) {
+        var keys,
+            i,
+            length;
+
+        if (string === "listKeys" || string === "array" || string === "buffer" || string === "i") {
+            return;
+        }
+
+        if (typeof(object) !== "object" || object === null) {
+            array.push(string + " " + typeof(object));
+            return;
+        }
+
+        keys = Object.keys(object);
+        for (i = 0, length = keys.length; i < length; i++) {
+            if (string === "") {
+                listKeys(keys[i], object[keys[i]]);
+            } else if (keys[i] !== "parent") {
+                listKeys(string + "." + keys[i], object[keys[i]]);
+            }
         }
     }
-}
 
-listKeys("", this);
-array.sort();
+    listKeys("", this);
+    array.sort();
 
-var buffer = "\n======= JS API list =======";
-for (var i = 0; i < array.length; ++i) {
-    buffer += "\n" + array[i];
-}
-buffer += "\n========= API END =========\n";
+    buffer = "\n======= JS API list =======";
+    for (i = 0, length = array.length; i < length; i++) {
+        buffer += "\n" + array[i];
+    }
+    buffer += "\n========= API END =========\n";
 
-print(buffer);
+    print(buffer);
+
+}());
