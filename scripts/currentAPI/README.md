effe0df5701a802312f7a03624e7c1181399da4e
David Rowe <david@ctrlaltstudio.com>
2019-06-24 22:12:43 +1200

    Make able to run as client and server entithy scripts

--- scripts/currentAPI/README.md
+++ scripts/currentAPI/README.md
@@ -1,7 +1,7 @@
 # currentAPI.js
 
-CtrlAltStudio's modified version of High Fidelity's original currentAPI.js script that writes the API to the program log (i.e., 
-prior to the UI being added).
+CtrlAltStudio's modified version of High Fidelity's original currentAPI.js script that writes the API to the log file (i.e., 
+prior to the UI being added to the script).
 
 Modifications:
 - ESLint.
@@ -9,5 +9,6 @@ Modifications:
 - List top-level objects.
 - List build version.
 - Code switch to list root items only.
+- Make work in all script contexts (i.e., Interface, client entity, etc.).
 
 [LICENSE](LICENSE)
--- scripts/currentAPI/currentAPI.js
+++ scripts/currentAPI/currentAPI.js
@@ -10,12 +10,28 @@ Modified by David Rowe:
 - List top-level objects.
 - List build version.
 - Code switch to list root items only.
+- Make able to run as client or server entity script.
 
 Distributed under the Apache License, Version 2.0.
 See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 */
 
-(function () {
+/* Client entity and server entity scripts require use of _this. */
+var _this = this;
+
+/* Interface, avatar, and assignment client scripts call function expression. */
+try {
+    if (Script.context === "client" || Script.context === "agent") {
+        Script.setTimeout(function () {
+            f();
+        }, 500);
+    }
+} catch (e) {
+    /* Script object isn't available when evaluating client entity and server entity scripts. */
+}
+
+/* Client entity and server entity script require function expression with no "var". */
+f = function () {
 
     "use strict";
 
@@ -27,10 +43,12 @@ See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.
 
     function listKeys(string, object) {
         var keys,
+            IGNORE_ITEMS = ["_this", "f"],
+            QT_IGNORE_ITEMS = ["destroyed", "objectName", "objectNameChanged"],
             i,
             length;
 
-        if (string === "listKeys" || string === "array" || string === "buffer" || string === "i") {
+        if (IGNORE_ITEMS.indexOf(string) !== -1) {
             return;
         }
 
@@ -42,23 +60,28 @@ See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.
         keys = Object.keys(object);
         for (i = 0, length = keys.length; i < length; i++) {
             if (string === "") {
-                if (object === this && typeof object[keys[i]] === "object") {
-                    array.push(keys[i] + " object"); // Top-level items
+                if (object === _this && typeof object[keys[i]] === "object" && keys[i] !== "_this") {
+                    array.push(keys[i] + " object"); /* Top-level item */
                 }
                 listKeys(keys[i], object[keys[i]]);
             } else if (keys[i] !== "parent"
                     && !ROOT_ITEMS_ONLY
-                    && ["destroyed", "objectName", "objectNameChanged"].indexOf(keys[i].split("(")[0]) === -1) { // Qt functions
+                    && QT_IGNORE_ITEMS.indexOf(keys[i].split("(")[0]) === -1) {
                 listKeys(string + "." + keys[i], object[keys[i]]);
             }
         }
     }
 
-    listKeys("", this);
+    listKeys("", _this);
     array.sort();
 
     buffer = "\n======= JS API list =======";
-    buffer += "\n Build version: " + HifiAbout.buildVersion + "\n";
+    buffer += "\n Script context: " + Script.context;
+    if (Script.context !== "entity_server" && Script.context !== "agent") {
+        /* HifiAbout is not available to server entity or assignment client scripts. */
+        buffer += "\n Build version: " + HifiAbout.buildVersion;
+    }
+    buffer += "\n";
     for (i = 0, length = array.length; i < length; i++) {
         buffer += "\n" + array[i];
     }
@@ -66,4 +89,4 @@ See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.
 
     print(buffer);
 
-}());
+};
